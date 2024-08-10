f9a0863a40ca82b8aed33a6a3c9b5f71730833e9
Shiva9361 <shivadharshansankar936@gmail.com>
2024-08-10 16:37:21 +0530

    feat: added base

---
+++ app/src/main/java/com/shiva936/nayidishavyapar/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.shiva936.nayidishavyapar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shiva936.nayidishavyapar.ui.theme.NayiDishaVyaparTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NayiDishaVyaparTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NayiDishaVyaparTheme {
+        Greeting("Android")
+    }
+}
