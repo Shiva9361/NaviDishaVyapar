4892e6a653fddb281fdfe486b59b3a994d4ae9aa
Shiva9361 <shivadharshansankar936@gmail.com>
2024-08-10 18:31:31 +0530

    feat: auth trial

--- app/src/main/java/com/shiva936/nayidishavyapar/MainActivity.kt
+++ app/src/main/java/com/shiva936/nayidishavyapar/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.shiva936.nayidishavyapar
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,11 +12,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.auth.FirebaseAuth
 import com.shiva936.nayidishavyapar.ui.theme.NayiDishaVyaparTheme
 
 class MainActivity : ComponentActivity() {
+    val auth :FirebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth.createUserWithEmailAndPassword("cs22b057@iittp.ac.in","Not my password").addOnCompleteListener {task ->
+        if (task.isSuccessful){
+            Toast.makeText(applicationContext,"Created",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            Toast.makeText(applicationContext,task.exception?.toString(),Toast.LENGTH_SHORT).show()
+        }
+        }
         enableEdgeToEdge()
         setContent {
             NayiDishaVyaparTheme {
---
+++ app/src/main/java/com/shiva936/nayidishavyapar/Users.kt
@@ -0,0 +1,5 @@
+package com.shiva936.nayidishavyapar
+
+data class Users(val id: String="",val name : String ="",
+                 val age : Int=0, var email :String="" ) {
+}
