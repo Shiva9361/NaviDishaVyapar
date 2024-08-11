2265a15f4701682ee8cfd0de8728a26c4e200d9e
Shiva9361 <shivadharshansankar936@gmail.com>
2024-08-11 13:12:35 +0530

    feat: added loginActivity

---
+++ app/src/main/java/com/shiva936/nayidishavyapar/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.shiva936.nayidishavyapar
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.ComponentActivity
+import com.shiva936.nayidishavyapar.LoginActivity
+
+class SplashScreenActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent = Intent(this@SplashScreenActivity,LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            },2000
+        )
+    }
+}
