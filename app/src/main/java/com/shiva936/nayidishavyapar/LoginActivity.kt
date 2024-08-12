18ec931539bf08960cf37b8a8350ba2badabeb99
Akileshdash <119668304+Akileshdash@users.noreply.github.com>
2024-08-13 01:32:40 +0530

    feat : Created Login Screen to be fixed : back arrow not visible in phone, some constratints are off screen

--- app/src/main/java/com/shiva936/nayidishavyapar/LoginActivity.kt
+++ app/src/main/java/com/shiva936/nayidishavyapar/LoginActivity.kt
@@ -11,30 +11,38 @@ import com.shiva936.nayidishavyapar.databinding.ActivityLoginBinding
  * Login Activity, Takes care of login
  * Uses firebase Auth in backend
  */
-class LoginActivity : ComponentActivity() {
-    private lateinit var loginBinding: ActivityLoginBinding
-    private val auth : FirebaseAuth = FirebaseAuth.getInstance()
+//class LoginActivity : ComponentActivity() {
+//    private lateinit var loginBinding: ActivityLoginBinding
+//    private val auth : FirebaseAuth = FirebaseAuth.getInstance()
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+//        val view = loginBinding.root
+//        enableEdgeToEdge()
+//        setContentView(view)
+//        loginBinding.login.setOnClickListener {
+//            auth.signInWithEmailAndPassword("cs22b057@iittp.ac.in", "Not my password")
+//                .addOnCompleteListener { task ->
+//                    if (task.isSuccessful) {
+//                        Toast.makeText(
+//                            applicationContext,
+//                            auth.currentUser?.uid,
+//                            Toast.LENGTH_SHORT
+//                        )
+//                            .show()
+//                        println("User ID: ${auth.currentUser?.uid}")
+//                    }
+//
+//                }
+//        }
+//    }
+//}
 
+
+class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
-        val view = loginBinding.root
-        enableEdgeToEdge()
-        setContentView(view)
-        loginBinding.login.setOnClickListener {
-            auth.signInWithEmailAndPassword("cs22b057@iittp.ac.in", "Not my password")
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(
-                            applicationContext,
-                            auth.currentUser?.uid,
-                            Toast.LENGTH_SHORT
-                        )
-                            .show()
-                        println("User ID: ${auth.currentUser?.uid}")
-                    }
-
-                }
-        }
+        setContentView(R.layout.activity_login_screen) // Set the layout to activity_login_screen.xml
     }
 }
