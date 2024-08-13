a1366e9ef98ccbd86a0a12f2de6ad069488b8f17
Shiva9361 <shivadharshansankar936@gmail.com>
2024-08-13 15:38:58 +0530

    feat: login and signup backend

--- app/src/main/java/com/shiva936/nayidishavyapar/LoginActivity.kt
+++ app/src/main/java/com/shiva936/nayidishavyapar/LoginActivity.kt
@@ -2,58 +2,52 @@ package com.shiva936.nayidishavyapar
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import com.google.firebase.auth.FirebaseAuth
-import com.shiva936.nayidishavyapar.databinding.ActivityLoginBinding
+import com.shiva936.nayidishavyapar.databinding.ActivityLoginScreenBinding
 
 /**
  * Login Activity, Takes care of login
  * Uses firebase Auth in backend
  */
-//class LoginActivity : ComponentActivity() {
-//    private lateinit var loginBinding: ActivityLoginBinding
-//    private val auth : FirebaseAuth = FirebaseAuth.getInstance()
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
-//        val view = loginBinding.root
-//        enableEdgeToEdge()
-//        setContentView(view)
-//        loginBinding.login.setOnClickListener {
-//            auth.signInWithEmailAndPassword("cs22b057@iittp.ac.in", "Not my password")
-//                .addOnCompleteListener { task ->
-//                    if (task.isSuccessful) {
-//                        Toast.makeText(
-//                            applicationContext,
-//                            auth.currentUser?.uid,
-//                            Toast.LENGTH_SHORT
-//                        )
-//                            .show()
-//                        println("User ID: ${auth.currentUser?.uid}")
-//                    }
-//
-//                }
-//        }
-//    }
-//}
-
-
 class LoginActivity : ComponentActivity() {
+    private lateinit var loginBinding: ActivityLoginScreenBinding
+    private val auth : FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_screen) // Set the layout to activity_login_screen.xml
-        // Find the button by its ID
-        val registerButton: Button = this.findViewById(R.id.register_with_us)
+
+        //ActivityNameBinding can be used to get the content from XML
+        loginBinding = ActivityLoginScreenBinding.inflate(layoutInflater)
+        val view = loginBinding.root
+        enableEdgeToEdge()
+        setContentView(view)
 
         // Set up the click listener
-        registerButton.setOnClickListener {
+        loginBinding.registerWithUs.setOnClickListener {
             // Create an Intent to start the SignupActivity
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+        }
+
+        loginBinding.Next.setOnClickListener{
+            val email = loginBinding.Email.text.toString()
+            val password = loginBinding.Password.text.toString()
+            if (email !="" && password !="" ){
+                auth.signInWithEmailAndPassword(email,password).addOnCompleteListener{ task->
+                    if (task.isSuccessful){
+                        Toast.makeText(applicationContext, auth.currentUser?.uid  ,Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        Toast.makeText(applicationContext, "Something went wrong!!"  ,Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            else{
+                Toast.makeText(applicationContext,"Please enter both email and password",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/shiva936/nayidishavyapar/SignUpActivity.kt
+++ app/src/main/java/com/shiva936/nayidishavyapar/SignUpActivity.kt
@@ -4,13 +4,69 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import android.graphics.Color
+import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
-import com.shiva936.nayidishavyapar.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.database.FirebaseDatabase
+import com.shiva936.nayidishavyapar.databinding.ActivitySignupScreenBinding
 
 
 class SignUpActivity : ComponentActivity() {
+    private lateinit var signupScreenBinding: ActivitySignupScreenBinding
+    private lateinit var selectedRole: String
+    private val auth : FirebaseAuth = FirebaseAuth.getInstance()
+    private val database :FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val userref = database.reference.child("user_data")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup_screen)
+        signupScreenBinding = ActivitySignupScreenBinding.inflate(layoutInflater)
+        val view = signupScreenBinding.root
+        val roles = listOf(signupScreenBinding.Buyer,signupScreenBinding.Agent,signupScreenBinding.Seller)
+        selectedRole = signupScreenBinding.Buyer.text.toString()
+        signupScreenBinding.Buyer.setBackgroundColor(Color.RED)
+
+        for (type in roles){
+            type.setOnClickListener{
+                switchSelectedType(type,roles)
+            }
+        }
+
+        setContentView(view)
+        enableEdgeToEdge()
+
+        signupScreenBinding.next.setOnClickListener{
+            val name = signupScreenBinding.Name.text.toString()
+            val email = signupScreenBinding.Email.text.toString()
+            val password = signupScreenBinding.Password.text.toString()
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty()){
+                Toast.makeText(applicationContext, "Please fill all details", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener{ task ->
+                    if (task.isSuccessful) {
+                        userref.child("${auth.currentUser?.uid}").child("role").setValue(selectedRole)
+                        userref.child("${auth.currentUser?.uid}").child("name").setValue(name)
+                    }
+                    else{
+                        if (task.exception is FirebaseAuthUserCollisionException){
+                            Toast.makeText(applicationContext, "Email is already registered", Toast.LENGTH_SHORT).show()
+                        }
+                        else{
+                            Toast.makeText(applicationContext, "${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    private fun switchSelectedType(role:Button,roles:List<Button>){
+        for (type in roles){
+            type.setBackgroundColor(Color.WHITE)
+        }
+        role.setBackgroundColor(Color.RED)
+        selectedRole = role.text.toString()
     }
 }
