a95e7fa4b15ecbb17dbf6cbb63bef51a160c62b8
Shiva9361 <shivadharshansankar936@gmail.com>
2024-08-13 17:45:45 +0530

    feat: dummy activity for checking

---
+++ app/src/main/java/com/shiva936/nayidishavyapar/DummyActivity.kt
@@ -0,0 +1,11 @@
+package com.shiva936.nayidishavyapar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class DummyActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_screen)
+    }
+}
