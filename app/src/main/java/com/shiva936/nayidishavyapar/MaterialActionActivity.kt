5c97f53d1f4605d7141c22909ace620e07cbd4df
Shiva9361 <shivadharshansankar936@gmail.com>
2024-08-14 09:53:53 +0530

    feat: Redirection based on login status

---
+++ app/src/main/java/com/shiva936/nayidishavyapar/MaterialActionActivity.kt
@@ -0,0 +1,19 @@
+package com.shiva936.nayidishavyapar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.shiva936.nayidishavyapar.databinding.ActivityMaterialActionBinding
+
+class MaterialActionActivity : ComponentActivity() {
+    private lateinit var materialActionBinding: ActivityMaterialActionBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        materialActionBinding = ActivityMaterialActionBinding.inflate(layoutInflater)
+        val view = materialActionBinding.root
+        setContentView(view)
+    }
+}
