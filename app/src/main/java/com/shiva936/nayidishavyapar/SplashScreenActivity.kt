13d4ada79a352223811445026846eeadeb7aa8a6
Shiva9361 <shivadharshansankar936@gmail.com>
2024-08-16 10:32:16 +0530

    feat: Splash Screen Backend

--- app/src/main/java/com/shiva936/nayidishavyapar/SplashScreenActivity.kt
+++ app/src/main/java/com/shiva936/nayidishavyapar/SplashScreenActivity.kt
@@ -7,21 +7,35 @@ import android.os.Handler
 import android.os.Looper
 import androidx.activity.ComponentActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : ComponentActivity() {
     private val auth : FirebaseAuth = FirebaseAuth.getInstance()
+    private val database : FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val userReference = database.reference.child("user_data")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         Handler(Looper.getMainLooper()).postDelayed(
             {
-                var intent = Intent(this@SplashScreenActivity, MaterialActionActivity::class.java)
+                var intent = Intent(this@SplashScreenActivity, MainMenuActivity::class.java)
                 if (auth.currentUser != null) {
-                    intent = Intent(this@SplashScreenActivity, MainMenuActivity::class.java)
+                    userReference.child(auth.currentUser!!.uid.toString()).child("location").get().addOnCompleteListener { task ->
+                        if (!task.isSuccessful || !task.result.exists()) {
+                            intent = Intent(this@SplashScreenActivity, LocationScreenActivity::class.java)
+                        }
+                        userReference.child(auth.currentUser!!.uid.toString()).child("material").get().addOnCompleteListener { task ->
+                            if (!task.isSuccessful || !task.result.exists()) {
+                                intent = Intent(this@SplashScreenActivity, MaterialTypeActivity::class.java)
+                            }
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+
                 }
-                startActivity(intent)
-                finish()// To prevent user from coming back by accident
             },2000
         )
     }
