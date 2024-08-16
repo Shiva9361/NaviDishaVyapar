10af78b812361c41f29d93e5794867cfa423469b
Shiva9361 <shivadharshansankar936@gmail.com>
2024-08-16 17:39:27 +0530

    feat: material types

--- app/src/main/java/com/shiva936/nayidishavyapar/SearchScreenActivity.kt
+++ app/src/main/java/com/shiva936/nayidishavyapar/SearchScreenActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.TypedValue.COMPLEX_UNIT_SP
 import android.util.TypedValue.applyDimension
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -21,6 +22,8 @@ class SearchScreenActivity : ComponentActivity() {
     private val userReference = database.reference.child("user_data")
     private lateinit var searchScreenBinding: ActivitySearchScreenBinding
     private val selectedCities = mutableSetOf<String>()
+    private val selectedMaterials = mutableListOf<String>()
+    private lateinit var selectedMethod : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,12 +31,40 @@ class SearchScreenActivity : ComponentActivity() {
 
         val view = searchScreenBinding.root
         setContentView(view)
+
+        val methods = listOf(searchScreenBinding.acquire,searchScreenBinding.lease,searchScreenBinding.request,searchScreenBinding.industrial)
+        selectedMethod = methods[0]
+        for (method in methods){
+            method.setOnClickListener{
+                for (method2 in methods){
+                    method2.setBackgroundColor(getColor(R.color.light_grey))
+                    method2.setTextColor(getColor(R.color.black))
+                }
+                method.setBackgroundResource(R.drawable.search_screen_button_background_selector)
+                method.setTextColor(getColor(R.color.grey))
+                selectedMethod = method
+            }
+        }
+        // Initializing here, changing in XML doesn't work :)
+        searchScreenBinding.frameOther.alpha = 0.5F
+        searchScreenBinding.frameAgriculture.alpha = 0.5F
+        searchScreenBinding.frameFood.alpha = 0.5F
+        searchScreenBinding.frameHealth.alpha = 0.5F
+        searchScreenBinding.frameMining.alpha = 0.5F
+        searchScreenBinding.frameOffice.alpha = 0.5F
+        searchScreenBinding.frameAuto.alpha = 0.5F
+        searchScreenBinding.frameManufacturing.alpha = 0.5F
+        searchScreenBinding.frameRetail.alpha = 0.5F
+        searchScreenBinding.frameHospitality.alpha = 0.5F
+
+
         searchScreenBinding.addMoreButton.setOnClickListener{
             // Can make this smoother afterwards
             intent = Intent(this@SearchScreenActivity,LocationScreenActivity::class.java)
             startActivity(intent)
             finish()
         }
+
         searchScreenBinding.resetButton.setOnClickListener{
             intent = Intent(this@SearchScreenActivity,SearchScreenActivity::class.java)
             startActivity(intent)
@@ -45,6 +76,48 @@ class SearchScreenActivity : ComponentActivity() {
 
         loadCities()
 
+        searchScreenBinding.frameAgriculture.setOnClickListener{
+            handelMaterialClick("Agriculture",searchScreenBinding.frameAgriculture)
+        }
+        searchScreenBinding.frameAuto.setOnClickListener{
+            handelMaterialClick("Auto",searchScreenBinding.frameAuto)
+        }
+        searchScreenBinding.frameFood.setOnClickListener{
+            handelMaterialClick("Food",searchScreenBinding.frameFood)
+        }
+        searchScreenBinding.frameHealth.setOnClickListener{
+            handelMaterialClick("Health",searchScreenBinding.frameHealth)
+        }
+        searchScreenBinding.frameOther.setOnClickListener{
+            handelMaterialClick("Other",searchScreenBinding.frameOther)
+        }
+        searchScreenBinding.frameMining.setOnClickListener{
+            handelMaterialClick("Mining",searchScreenBinding.frameMining)
+        }
+        searchScreenBinding.frameHospitality.setOnClickListener{
+            handelMaterialClick("Hospitality",searchScreenBinding.frameHospitality)
+        }
+        searchScreenBinding.frameManufacturing.setOnClickListener{
+            handelMaterialClick("Manufacturing",searchScreenBinding.frameManufacturing)
+        }
+        searchScreenBinding.frameOffice.setOnClickListener{
+            handelMaterialClick("Office",searchScreenBinding.frameOffice)
+        }
+        searchScreenBinding.frameRetail.setOnClickListener{
+            handelMaterialClick("Office",searchScreenBinding.frameRetail)
+        }
+
+
+    }
+
+    private fun handelMaterialClick(s: String, frameLayout: FrameLayout) {
+        if (selectedMaterials.contains(s)) {
+            selectedMaterials.remove(s)
+            frameLayout.alpha = 0.5F
+        } else {
+            selectedMaterials.add(s)
+            frameLayout.alpha = 1F
+        }
     }
 
     private fun loadCities() {
@@ -54,7 +127,7 @@ class SearchScreenActivity : ComponentActivity() {
                 for (buttonSnapshot in snapshot.children) {
                     val text = buttonSnapshot.key
                     if (text != null) {
-                        createButton(text)
+                        createButton(text,selectedCities)
                     }
                 }
             }
@@ -64,13 +137,14 @@ class SearchScreenActivity : ComponentActivity() {
             }
         })
     }
-    private fun createButton(text: String) {
+    private fun createButton(text: String, strings: MutableSet<String>) {
         val button = Button(this@SearchScreenActivity)
         button.text = text
-        selectedCities.add(text)
+        strings.add(text)
         val params = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,  // Width
             spToPx(40F)
+
         ).apply {
             setMargins(5, 5, 5, 10) // Set margins
         }
@@ -79,14 +153,16 @@ class SearchScreenActivity : ComponentActivity() {
         button.setTextColor(getColor(R.color.dark_green))
         button.setBackgroundResource(R.drawable.green_background_rounded)
         button.setOnClickListener{
-            if (selectedCities.contains(text)){
-                selectedCities.remove(text)
+            if (strings.contains(text)){
+                strings.remove(text)
                 button.setBackgroundResource(R.drawable.baseline_electric_meter_24)
+                button.setTextColor(getColor(R.color.black))
 
             }
             else{
-                selectedCities.add(text)
+                strings.add(text)
                 button.setBackgroundResource(R.drawable.green_background_rounded)
+                button.setTextColor(getColor(R.color.dark_green))
             }
         }
         searchScreenBinding.locationGrid.addView(button,0)
