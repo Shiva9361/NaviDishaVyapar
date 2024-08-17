52e7072bfa7c3f9fd668127efeb53d67da64e6eb
VINEETH <svraghavvendra@gmail.com>
2024-08-17 19:56:55 +0530

    fix : added some code in SellScreen3Activity.kt,activity_sell_screen_3.xml,AndroidManifest.xml

--- app/src/main/java/com/shiva936/nayidishavyapar/SellScreen3Activity.kt
+++ app/src/main/java/com/shiva936/nayidishavyapar/SellScreen3Activity.kt
@@ -1,15 +1,54 @@
-package com.shiva936.nayidishavyapar
-
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.shiva936.nayidishavyapar.databinding.ActivitySellScreen3Binding
 
-class SellScreen3Activity : ComponentActivity() {
+class SellScreen3Activity : AppCompatActivity() {
+
     private lateinit var sellScreen3Binding: ActivitySellScreen3Binding
+    private val LOCATION_REQUEST_CODE = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         sellScreen3Binding = ActivitySellScreen3Binding.inflate(layoutInflater)
         val view = sellScreen3Binding.root
         setContentView(view)
+
+
+        sellScreen3Binding.locationImageView101.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_REQUEST_CODE)
+            } else {
+                enableLocation()
+            }
+        }
+    }
+
+    private fun enableLocation() {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            Toast.makeText(this, "Please enable GPS", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "GPS is already enabled", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableLocation()
+            } else {
+                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
