20822630f4b6794df8c9298b9e7bed47ad65b62f
Shiva9361 <shivadharshansankar936@gmail.com>
2024-08-19 15:37:44 +0530

    fix: sell screen 5

--- app/src/main/java/com/shiva936/nayidishavyapar/SellScreen5Activity.kt
+++ app/src/main/java/com/shiva936/nayidishavyapar/SellScreen5Activity.kt
@@ -1,9 +1,11 @@
 package com.shiva936.nayidishavyapar
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.storage.FirebaseStorage
 import com.shiva936.nayidishavyapar.databinding.ActivitySellScreen5Binding
 import java.util.UUID
@@ -11,21 +13,47 @@ import java.util.UUID
 class SellScreen5Activity : ComponentActivity() {
     private val storage = FirebaseStorage.getInstance()
     private val storageRef = storage.reference
-
+    private val images = arrayListOf<String>()
+    private val pickImageLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        uri?.let {
+            uploadImageToFirebase(it)
+        }
+    }
     private lateinit var sellScreen5Binding: ActivitySellScreen5Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         sellScreen5Binding = ActivitySellScreen5Binding.inflate(layoutInflater)
         val view = sellScreen5Binding.root
         setContentView(view)
+        sellScreen5Binding.buttonUploadImage.setOnClickListener {
+            pickImageLauncher.launch("image/*")
+        }
+        sellScreen5Binding.next.setOnClickListener {
+            val childIntent = Intent(this@SellScreen5Activity,SellScreen6Activity::class.java)
+            if (intent.extras!=null){
+                childIntent.putExtras(intent.extras!!)
+            }
+            if (images.isNotEmpty()){
+                childIntent.putStringArrayListExtra("Images",ArrayList(images))
+                startActivity(childIntent)
+            }
+            else{
+                Toast.makeText(applicationContext, "Upload at least one Image", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
     private fun uploadImageToFirebase(imageUri: Uri) {
-        val imageRef = storageRef.child("images/${UUID.randomUUID()}.jpg")
+        val imageName = UUID.randomUUID()
+        val imageRef = storageRef.child("images/${imageName}.jpg")
+
 
         imageRef.putFile(imageUri)
             .addOnSuccessListener {
                 // Image uploaded successfully
                 Toast.makeText(this, "Upload successful!", Toast.LENGTH_SHORT).show()
+                images.add(imageName.toString())
             }
             .addOnFailureListener {
                 // Handle any errors
